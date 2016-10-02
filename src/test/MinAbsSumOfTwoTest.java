8e9f19177c99c01733d5712b11acdc6f9d2fc551
giss-ignacio <giss.ignacio@gmail.com>
2016-10-02 20:12:56 -0300

    MinAbsSumOfTwoTest

---
+++ src/test/MinAbsSumOfTwoTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import main.MinAbsSumOfTwo;
+
+import static org.junit.Assert.assertEquals;
+
+public class MinAbsSumOfTwoTest {
+    @org.junit.Test
+    public void TestBasic() {
+        MinAbsSumOfTwo sol = new MinAbsSumOfTwo();
+        int[] A = {1, 4, -3};
+        assertEquals(1, sol.solution(A));
+    }
+    @org.junit.Test
+    public void TestOne() {
+        MinAbsSumOfTwo sol = new MinAbsSumOfTwo();
+        int[] A = {-8, 4, 5, -10, 3};
+        assertEquals(3, sol.solution(A));
+    }
+	@org.junit.Test
+    public void TestTwo() {
+        MinAbsSumOfTwo sol = new MinAbsSumOfTwo();
+        int[] A = {-5, -3, -1, 0, 3, 6};
+        assertEquals(0, sol.solution(A));
+    }
+	@org.junit.Test
+    public void TestAllPositive() {
+        MinAbsSumOfTwo sol = new MinAbsSumOfTwo();
+        int[] A = {0, 11, 5, 2, 19};
+        assertEquals(2, sol.solution(A));
+    }
+	@org.junit.Test
+    public void TestAllNegative() {
+        MinAbsSumOfTwo sol = new MinAbsSumOfTwo();
+        int[] A = {-5, -1, -7, -3}
+        assertEquals(4, sol.solution(A));
+    }
+}
